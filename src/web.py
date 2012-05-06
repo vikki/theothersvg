4ff7c368a15b3b1083dd807f67fec28c7502e2df
Vikki Read <vikki@Vikki-Reads-MacBook-Pro.local>
2012-05-07 00:51:13 +0100

    fixing location of static resources

--- src/web.py
+++ src/web.py
@@ -5,7 +5,7 @@ import urllib2
 import os
 from jinja2 import FileSystemLoader
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 @app.route("/")
 def securityProxyForHTML():
@@ -13,5 +13,5 @@ def securityProxyForHTML():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.jinja_loader = FileSystemLoader('templates')
+    #app.jinja_loader = FileSystemLoader('templates')
     app.run(host='0.0.0.0', port=port, debug=True)
